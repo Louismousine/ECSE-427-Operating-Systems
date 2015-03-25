707bb3882e4b9dd92573197e9807d0b534e2184f
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-03-25 15:28:07 -0400

    .

--- Assignment3/sfs_api.c
+++ Assignment3/sfs_api.c
@@ -542,9 +542,10 @@ int sfs_fread(int fileID, char *buf, int length) //returns -1 for failure
 
   fileDescriptorEntry *readFile = descriptorTable[fileID];
 
-
+  fprintf(stderr, "fileID %d\n numfiles %d\n length %d\n", fileID, numFiles, length);
+  fprintf(stderr, "inode:%d\n", writeFile->inode);
+  fprintf(stderr, "inode size:%d\n", inode->size);
   inodeEntry *inode = &(inodeTable[readFile->inode]);
-
   if(readFile->readPointer + length > inode->size)
   {
     length = inode->size - readFile->readPointer;
