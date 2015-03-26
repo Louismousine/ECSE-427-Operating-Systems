32bf01d98f3d8f8821eaca8df9de6410769738f9
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-03-26 16:04:26 -0400

    .

--- Assignment3/sfs_test.c
+++ Assignment3/sfs_test.c
@@ -131,9 +131,9 @@ main(int argc, char **argv)
       free(buffer);
     }
   }
-  char *readTest = (char*)malloc(filesize[0]);
-  sfs_fread(0, readTest, filesize[0]);
-  fprintf(stderr, "%s/n", readTest);
+  char readTest[filesize[0]];
+  sfs_fread(0, &readTest, filesize[0]);
+  fprintf(stderr, "%s\n", readTest);
   if (sfs_fclose(fds[1]) != 0) {
     fprintf(stderr, "ERROR: close of handle %d failed\n", fds[1]);
     error_count++;
