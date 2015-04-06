e305f0ff967c5270fb03746e418779a7b4cc19ff
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-04-06 16:15:22 -0400

    .

--- malloc_test.c
+++ malloc_test.c
@@ -37,33 +37,33 @@ int main(int argc, char* argv[])
   my_free(buffer);
   free(buffer_check);
 
-  buffer = my_malloc(512);
-  buffer_check = malloc(512);
+  int *buffer2 = my_malloc(512);
+  int buffer_check2 = malloc(512);
   for(i = 0; i < 512/sizeof(int); i++)
   {
-    buffer[i] = i+k;
+    buffer2[i] = i+k;
     k++;
     k++;
   }
   k = 0;
   for(i = 0; i < 512/sizeof(int); i++)
   {
-    buffer_check[i] = i+k;
+    buffer_check2[i] = i+k;
     k++;
     k++;
   }
   fprintf(stdout, "buffer: \n");
   for(i = 0; i < 512/sizeof(int); i++)
   {
-    fprintf(stdout, "%d\n", buffer[i]);
-    if(buffer_check[i] != buffer[i])
+    fprintf(stdout, "%d\n", buffer2[i]);
+    if(buffer_check[i] != buffer2[i])
     {
       fprintf(stderr, "Error, buffer does not match expected result\n");
       return -1;
     }
   }
-  my_free(buffer);
-  free(buffer_check);
+  my_free(buffer2);
+  free(buffer_check2);
   // if(buffer != NULL && buffer_check != NULL)
   // {
   //   fprintf(stderr, "Error freeing buffer");
