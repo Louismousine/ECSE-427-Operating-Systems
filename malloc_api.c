48370b782ae14ee4c5f49b7e1d52aae4d37abe90
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-04-08 14:32:32 -0400

    .

--- malloc_api.c
+++ malloc_api.c
@@ -399,7 +399,7 @@ void my_free(void *ptr)
     return;
   freeListNode *new = (freeListNode*)(((char*)ptr) - sizeof(freeListNode));
   new->next = freeListHead.next;
-  fprintf(stdout, "next startTag: %p\nnext endTag: %p\n", freeListHead.next->startTag, freeListHead.next->endTag);
+  fprintf(stdout, "next startTag: %p\nnext endTag: %p\n", new->next->startTag, new->next->endTag);
 
   freeSpace += new->size;
   bytesAlloc -= new->size;
@@ -407,7 +407,7 @@ void my_free(void *ptr)
   if(new->next != NULL)
     new->next->prev = new;
 
-  new->prev = &(freeListHead);
+  //new->prev = &(freeListHead);
 
   // if(new->prev != NULL)
   //   new->prev->next = new;
